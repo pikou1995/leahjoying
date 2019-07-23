ef6f363e1cc33b232b0cb4543870d1aba226d852
pikou <434708082@qq.com>
2019-07-24 01:12:41 +0800

    first commit

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">{{text}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: '爱在生长'
+    }
+  }
+};
+</script>
+
+<style>
+#test {
+  font-size: 12px;
+  color: green;
+}
+</style>
