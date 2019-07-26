1a9d10171d4e5972b5e0fe86d53430b3392343a7
pikou <434708082@qq.com>
2019-07-27 00:24:47 +0800

    计时

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,43 @@
 <template>
-  <div id="app">{{text}}</div>
+  <div id="app">
+    <p>爱已生长{{time}}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      text: '爱在生长'
+      startTime: '2019-05-01T18:40:00',
+      now: Date.now(),
+      timeHandler: null,
     }
-  }
-};
-</script>
-
-<style>
-#test {
-  font-size: 12px;
-  color: green;
+  },
+  computed: {
+    startTimestamp() {
+      return Date.parse(this.startTime)
+    },
+    time() {
+      let delta = this.now - this.startTimestamp
+      let days = Math.floor(delta / 1000 / 60 / 60 / 24)
+      let hours = Math.floor(delta / 1000 / 60 / 60 % 24)
+      let minutes = Math.floor(delta / 1000 / 60 % 60)
+      let seconds = Math.floor(delta / 1000 % 60)
+      return `${days}天${hours}时${minutes}分${seconds}秒`
+    },
+  },
+  methods: {
+    startTick() {
+      this.timeHandler = setInterval(() => {
+        this.now = Date.now()
+      }, 1000)
+    },
+    stopTick() {
+      clearInterval(this.timeHandler)
+    }
+  },
+  mounted() {
+    this.startTick() 
+  },
 }
-</style>
+</script>
